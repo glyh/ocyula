c582a23727f053b40d73f2dfbae46ce6c93c32c8
glyh <lyhokia@gmail.com>
2023-09-06 20:31:46 +0800

    refactor ast series: don't use GADT as it complicate the implementation

--- src/ocyula_lib/Ast.ml
+++ src/ocyula_lib/Ast.ml
@@ -1,11 +1,8 @@
 exception Unimplemented
-
-type ident = string
-
-type unary_operator = 
+type un_op = 
      NOT
 
-type binary_operator = 
+type bin_op = 
    | EQ | NE | LE | LT | GE | GT
    | ADD | SUB | MUL | DIV
    | AND | OR
@@ -19,223 +16,61 @@ type atom =
    | Bool of bool
    (* TODO: | Type of type_t *)
 
-type yes = Yes
-type no = No
+type value =
+   | Atom of atom
+   | List of value list
+   | Tuple of value list
 
-type upd_pat = UpdatablePattern
-type pat = Pattern
-type exp = Expression
+type ident = 
+   | Concrete of string
+   | Gensym of int
 
-(* Random: we may extend GADT by allowing same constructor to be overloaded *)
-(* https://icfp23.sigplan.org/details/ocaml-2023-papers/4/Modern-DSL-compiler-architecture-in-OCaml-our-experience-with-Catala *)
-type ('ty, 'dep, 'sur) flex_ast = 
+type updatable = 
+   | Bind of ident
+   | Lens of updatable * ident * exp list
+             (* obj    . method  (arg1, arg2, ...) *)
+and pat = 
+   | Updatable of updatable
+   | Union of pat list
+   | With of pat * exp
+   (* NOTE:
+      we have some ADT:
+      type f = 
+      | Left of int
+      | Right of unit
+      Left i | Right () with (i = 3) = Right()
+      exists solely for balancing the variables on both side of a union pattern
+   *)
+   | Pin of ident
+   | PatTuple of pat list
+   | PatList of pat list
+   | PLit of atom
+   | PAny
 
-   (* UPDATABLE PATTERN *)
+and exp = 
+   | Assert of exp
+   | Val of ident
+   | Lit of atom
+   | Bin of exp * bin_op * exp
+   | Un of un_op * exp
+   | Seq of exp list
+   | If of exp * exp * exp
+   | Tuple of exp list
+   | List of exp list
+   | Lambda of pat list * exp
+   | Call of ident * exp list 
+   | BindMatch of pat * exp
+   | UnPin of ident
+   | CaseMatch of exp * (pat * exp * exp) list
+   | Annotate of exp * Type.ty
 
-   | Bind: ident 
-   -> (upd_pat, 'dep, <pattern: yes; ..> as 'sur) flex_ast 
-   | Lens: 
-   (* obj                  . method  (arg1, arg2, ...) *) 
-   (upd_pat, 'dep) gen_ast * ident * (exp, 'dep) gen_ast list 
-   -> (upd_pat, 'dep, <pattern: yes; ..> as 'sur) flex_ast
+let gensym =
+   let counter = ref 1 in 
+      fun () -> 
+         counter := !counter + 1;
+         !counter - 1
 
-
-   (* PATTERN *)
-
-   | Union: 
-   (* rhs matches any of LHS, we should require LHS have same bindings and 
-      corresponding bindings have same type *)
-   (pat, 'dep) gen_ast list 
-   -> (pat, 'dep, <pattern: yes; ..> as 'sur) flex_ast
-   
-   | Updatable: 
-   (upd_pat, 'dep) gen_ast
-   -> (pat, 'dep, <pattern: yes; ..> as 'sur) flex_ast
-   
-   | Pin: (* match with exisiting value *)
-   ident
-   -> (pat, 'dep, <pattern: yes; ..> as 'sur) flex_ast
-
-   | PatTuple: 
-   (pat, 'dep) gen_ast list
-   -> (pat, 'dep, <pattern: yes; ..> as 'sur) flex_ast
-
-   | PatList: 
-   (pat, 'dep) gen_ast list
-   -> (pat, 'dep, <pattern: yes; ..> as 'sur) flex_ast
-
-   | Lit:
-   atom
-   -> (pat, 'dep, <pattern: yes; ..> as 'sur) flex_ast
-
-   | Any:
-   unit
-   -> (pat, 'dep, <pattern: yes; ..> as 'sur) flex_ast
-
-
-   (* EXPRESSION *)
-
-   | Val: 
-   ident 
-   -> (exp, 'dep, 'sur) flex_ast
-
-   | Atom:
-   atom
-   -> (exp, 'dep, 'sur) flex_ast
-
-   | Binary:
-   binary_operator * (exp, 'dep) gen_ast * (exp, 'dep) gen_ast
-   -> (exp, 'dep, 'sur) flex_ast
-
-   | Unary:
-   unary_operator * (exp, 'dep) gen_ast
-   -> (exp, 'dep, 'sur) flex_ast
-
-   | Fix:
-   (exp, 'dep) gen_ast
-   -> (exp, 'dep, 'sur) flex_ast
-
-   | Seq:
-   (exp, 'dep) gen_ast list
-   -> (exp, 'dep, 'sur) flex_ast
-
-   | If:
-   (exp, 'dep) gen_ast * (exp, 'dep) gen_ast * (exp, 'dep) gen_ast
-   -> (exp, 'dep, <seq: no; ..> as 'sur) flex_ast
-   | IfSeq:
-   (exp, 'dep) gen_ast * (exp, 'dep) gen_ast list * (exp, 'dep) gen_ast list
-   -> (exp, 'dep, <seq: yes; ..> as 'sur) flex_ast
-
-   | Tuple: 
-   (exp, 'dep) gen_ast list
-   -> (exp, 'dep, <compound_type: yes; ..> as 'sur) flex_ast
-
-   | List: 
-   (exp, 'dep) gen_ast list
-   -> (exp, 'dep, <compound_type: yes; ..> as 'sur) flex_ast
-
-   | Abs:
-   ident * (exp, 'dep) gen_ast
-   -> (exp, 'dep, <lambda: no; ..> as 'sur) flex_ast
-
-   | Lam:
-   (ident list) * (exp, 'dep) gen_ast
-   -> (exp, 'dep, <lambda: yes; seq: no; pattern: no; ..> as 'sur) flex_ast
-   | LamPat:
-   ((pat, 'dep) gen_ast list) * (exp, 'dep) gen_ast
-   -> (exp, 'dep, <lambda: yes; seq: no; pattern: yes; ..> as 'sur) flex_ast
-   | LamPatSeq:
-   ((pat, 'dep) gen_ast list) * (exp, 'dep) gen_ast list
-   -> (exp, 'dep, <lambda: yes; seq: yes; pattern: yes; ..> as 'sur) flex_ast
-
-   | App: 
-   (exp, 'dep) gen_ast * (exp, 'dep) gen_ast 
-   -> (exp, 'dep, <call: no; ..> as 'sur) flex_ast
-
-   | Call: 
-   ident * (exp, 'dep) gen_ast list
-   -> (exp, 'dep, <call: yes; ..> as 'sur) flex_ast
-
-   | LetIn: (* we may need fix to create mutual recursive bindings here so a let may have more than one output *)
-   ident list * (exp, 'dep) gen_ast * (exp, 'dep) gen_ast 
-   -> (exp, 'dep, <letin: yes; ..> as 'sur) flex_ast
-
-   | Match:
-   (pat, 'dep) gen_ast * (exp, 'dep) gen_ast
-   -> (exp, 'dep, <letin: no; pattern: yes; ..> as 'sur) flex_ast
-
-   | UnPin:
-   ident
-   -> (exp, 'dep, <unpin: yes; ..> as 'sur) flex_ast
-
-   | ConcreteCaseMatch: 
-   (* exp to match *)
-   (exp, 'dep) gen_ast *
-   (* pat  guard                 ret *)
-   (atom * (exp, 'dep) gen_ast * (exp, 'dep) gen_ast) list *
-   (exp, 'dep) gen_ast option ->
-   (exp, 'dep, <pattern: no; seq: no; ..> as 'sur) flex_ast
-
-   | CaseMatch: 
-   (exp, 'dep) gen_ast *
-   ((pat, 'dep) gen_ast * (exp, 'dep) gen_ast * (exp, 'dep) gen_ast) list ->
-   (exp, 'dep, <pattern: yes; seq: no; ..> as 'sur) flex_ast
-
-   | CaseMatchSeq:
-   (exp, 'dep) gen_ast *
-   ((pat, 'dep) gen_ast * (exp, 'dep) gen_ast * (exp, 'dep) gen_ast list) list ->
-   (exp, 'dep, <pattern: yes; seq: yes; ..> as 'sur) flex_ast
-
-and ('ty, 'a) gen_ast = ('ty, 'a, 'a) flex_ast
-
-type top = <
-   seq: yes;
-   pattern: yes;
-   compound_type: yes;
-   lambda: yes;
-   call: yes;
-   letin: no;
-   unpin: yes>
-
-type btm = <
-   seq: no;
-   pattern: no;
-   compound_type: no;
-   lambda: no;
-   call: no;
-   letin: no;
-   unpin: no>
-
-type ('src, 'dst) desugarer = { f: 't. ('t, 'src) gen_ast -> ('t, 'dst) gen_ast }
-
-let shallow_map
-   (type ty src dst)
-   (f: (src, dst) desugarer)
-   (e: (ty, src, dst) flex_ast)
-   : (ty, dst) gen_ast =
-   match e with 
-   (* updatable pattern *)
-   | Bind(id) -> Bind(id)
-   | Lens(obj, _method, args) -> 
-      Lens(f.f obj, _method, List.map f.f args)
-   (* pattern *)
-   | Union(alts) -> Union(List.map f.f alts)
-   | Updatable(u) -> Updatable(f.f u)
-   | Pin(id) -> Pin(id)
-   | PatTuple(ps) -> PatTuple(List.map f.f ps)
-   | PatList(ps) -> PatList(List.map f.f ps)
-   | Lit(a) -> Lit(a)
-   | Any() -> Any()
-
-   (* expression *)
-   | Val(v) -> Val(v)
-   | Atom(a) -> Atom(a)
-   | Binary(op, lhs, rhs) -> Binary(op, f.f lhs, f.f rhs)
-   | Unary(g, x) -> Unary(g, f.f x)
-   | Fix(e) -> Fix(f.f e)
-   | Seq(es) -> Seq(List.map f.f es)
-   | If(test, _then, _else) -> If(f.f test, f.f _then, f.f _else)
-   | IfSeq(test, _then, _else) -> IfSeq(f.f test, List.map f.f _then, List.map f.f _else)
-   | Tuple(es) -> Tuple(List.map f.f es)
-   | List(es) -> List(List.map f.f es)
-   | Abs(id, e) -> Abs(id, f.f e)
-   | Lam(ids, e) -> Lam(ids, f.f e)
-   | LamPat(pats, e) -> LamPat(List.map f.f pats, f.f e)
-   | LamPatSeq(pats, es) -> LamPatSeq(List.map f.f pats, List.map f.f es)
-   | App(g, x) -> App(f.f g, f.f x)
-   | Call(id, es) -> Call(id, List.map f.f es)
-   | LetIn(ids, _val, inner) -> LetIn(ids, f.f _val, f.f inner)
-   | Match(lhs, rhs) -> Match(f.f lhs, f.f rhs)
-   | UnPin(id) -> UnPin(id)
-   | ConcreteCaseMatch(e, branches, _else) ->
-      let f_branch (_val, guard, ret) = (_val, f.f guard, f.f ret) in 
-      begin match _else with
-         | Some(has_else) -> ConcreteCaseMatch(f.f e, List.map f_branch branches, Some(f.f has_else))
-         | _ -> raise Unimplemented
-      end
-   | CaseMatch(e, branches) ->
-      let f_branch (pat, guard, ret) = (f.f pat, f.f guard, f.f ret) in
-      CaseMatch(f.f e, List.map f_branch branches)
-   | CaseMatchSeq(e, branches) ->
-      let f_branch (pat, guard, ret) = (f.f pat, f.f guard, List.map f.f ret) in
-      CaseMatchSeq(f.f e, List.map f_branch branches)
+let rec gensyms n = 
+   if n == 0 
+   then []
+   else gensym() :: gensyms (n - 1)
--- src/ocyula_lib/Parser.mly
+++ src/ocyula_lib/Parser.mly
@@ -1,5 +1,6 @@
 %{
 open Ast
+
 exception WrongPatternFormat
 
 let rec break_last (l: 'a list) : ('a list * 'a) option = 
@@ -7,20 +8,21 @@ let rec break_last (l: 'a list) : ('a list * 'a) option =
   | [] -> None
   | [x] -> Some ([], x)
   | x :: rest -> 
-    match break_last rest with
+    begin match break_last rest with
     | Some (l, last) -> Some (x :: l, last)
     | None -> None
+    end
 
-let rec call_to_lens (_method: string) (args: (exp, top) gen_ast list) = match break_last args with
+let rec call_to_lens (_method: ident) (args: exp list) = match break_last args with
    | Some (but_last, last) -> 
       begin match exp_to_pat last with
       | Updatable u -> Updatable(Lens(u, _method, but_last))
       | _ -> raise WrongPatternFormat
       end
    | _ -> raise WrongPatternFormat
-and exp_to_pat (e: (exp, top) gen_ast) = 
+and exp_to_pat (e: exp) = 
    match e with
-   | Atom a -> Lit a
+   | Lit a -> PLit a
    | UnPin id -> Pin id
    | Val id -> Updatable(Bind id)
    | Tuple t -> PatTuple (List.map exp_to_pat t)
@@ -95,8 +97,8 @@ and exp_to_pat (e: (exp, top) gen_ast) =
 %nonassoc T_NOT
 %nonassoc T_PIN
 
-%type <(exp, top) gen_ast> program
-%type <unary_operator> un_op
+%type <exp> program
+//%type <unary_operator> un_op
 
 %start program
 
@@ -112,17 +114,17 @@ program:
 
 // Satement-like expressions
 exp: 
-  | IF test=exp _then=no_end_terminated_exps ELSE _else=end_terminated_exps { IfSeq(test, _then, _else) }
+  | IF test=exp _then=no_end_terminated_exps ELSE _else=end_terminated_exps { If(test, Seq(_then), Seq(_else)) }
   | MATCH matched=exp4 branches=list(case_param) ow=option(else_branch) END { 
     match ow with 
-    | Some(ow) -> CaseMatchSeq(matched, branches @ [ow]) 
-    | None -> CaseMatchSeq(matched, branches) 
+    | Some(ow) -> CaseMatch(matched, Seq(branches @ [ow])) 
+    | None -> CaseMatch(matched, Seq(branches))
   }
   | FUNCTION LPAREN args=separated_list(COMMA, exp4) RPAREN body=end_terminated_exps { 
-    LamPatSeq(List.map exp_to_pat args, body)
+    Lambda(List.map exp_to_pat args, Seq(body))
   }
   | FUNCTION name=IDENT LPAREN args=separated_list(COMMA, exp4) RPAREN body=end_terminated_exps { 
-    Match(Updatable(Bind name), LamPatSeq(List.map exp_to_pat args, body))
+    BindMatch(Updatable(Bind (Concrete name)), Lambda(List.map exp_to_pat args, Seq(body)))
   }
   | DO exps=end_terminated_exps { Seq(exps) }
   | exp1 { $1 }
@@ -131,7 +133,7 @@ case_param:
   | CASE pat=exp4 g=option(guard) act=no_end_terminated_exps { 
     match g with 
     | Some(g) -> (exp_to_pat pat, g, act) 
-    | None -> (exp_to_pat pat, Atom (Bool true), act)
+    | None -> (exp_to_pat pat, Lit (Bool true), act)
   }
 
 guard: 
@@ -139,7 +141,7 @@ guard:
 
 else_branch: 
   | ELSE act=no_end_terminated_exps {
-    (Any(), Atom (Bool true), act)
+    (PAny, Lit (Bool true), act)
   }
 
 no_end_terminated_exps: 
@@ -154,7 +156,7 @@ end_terminated_exps:
 exp1: 
   | pat=exp4 op=T_UPDATE_MATCH rhs=exp3 { 
     match exp_to_pat pat with
-    | Updatable u -> Match (Updatable u, Call(op, [pat; rhs]))
+    | Updatable u -> BindMatch (Updatable u, Call(Concrete op, [pat; rhs]))
     | _ -> raise WrongPatternFormat
   }
   | exp3 { $1 }
@@ -162,29 +164,29 @@ exp1:
 // "Expression-like" expressions, all rules conflicts with do block.
 exp3: 
   | pat=exp4 T_MATCH rhs=exp3 {
-    Match(exp_to_pat pat, rhs)
+    BindMatch(exp_to_pat pat, rhs)
   }
   | e1=exp3 op=bin_op e2=exp3 { 
-    Binary(op, e1, e2)
+    Bin(e1, op, e2)
   }
   | op=un_op e=exp3 { 
-    Unary(op, e)
+    Un(op, e)
   }
   | T_PIN id=IDENT {
-    UnPin(id)
+    UnPin(Concrete id)
   }
   | exp4 { $1 }
 
 // Expressions that have higher precedence than DOT
 exp4: 
-  | obj=exp4 DOT _method=CALL args=separated_list(COMMA, exp) RPAREN { Call(_method, args @ [obj]) }
+  | obj=exp4 DOT _method=CALL args=separated_list(COMMA, exp) RPAREN { Call(Concrete _method, args @ [obj]) }
   // above rule conflicts with binary/unary operation
-  | id=CALL l=separated_list(COMMA, exp) RPAREN { Call(id, l) }
+  | id=CALL l=separated_list(COMMA, exp) RPAREN { Call(Concrete id, l) }
   | LPAREN first=exp COMMA l=separated_nonempty_list(COMMA, exp) RPAREN { Tuple(first :: l) }
   | LBRACKET l=separated_list(COMMA, exp) RBRACKET { List(l) }
   | LPAREN e=exp RPAREN {e}
-  | a=atom { Atom(a) }
-  | id=IDENT { Val(id) }
+  | a=atom { Lit(a) }
+  | id=IDENT { Val(Concrete id) }
   | LPAREN RPAREN { Tuple([]) }
 
 %inline bin_op: 
